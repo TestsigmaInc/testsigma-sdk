23ea98e5457d6557801b7cb87cf3c51f183a0186
vikram <vikram@testsigma.com>
2021-06-11 02:58:07 +0530

    find element methods added

--- src/main/java/com/testsigma/sdk/UIIdentifier.java
+++ src/main/java/com/testsigma/sdk/UIIdentifier.java
@@ -3,11 +3,23 @@ package com.testsigma.sdk;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 @RequiredArgsConstructor
 @Data
 public class UIIdentifier {
-  private final String value;
-  private final By by;
-  private WebElement element;
+    private final String value;
+    private final By by;
+    private WebElement element;
+
+    public void findElement(RemoteWebDriver driver) {
+        this.element = driver.findElement(by);
+    }
+    public WebElement getElement(RemoteWebDriver driver) {
+        if(element == null) {
+            findElement(driver);
+        }
+        return element;
+    }
 }
